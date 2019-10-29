185f69ab925e1a25077a0e1daca913a622cf0a7a
Gustavo Fette <gustavofette@gmail.com>
2019-10-29 13:25:23 -0300

    Made the clima1.py better on Tue Oct 29 13:25:23 -03 2019

--- clima1.py
+++ clima1.py
@@ -6,8 +6,8 @@ weather_url = "http://api.openweathermap.org/data/2.5/weather"
 city = input("Please choose your city:")
 
 response = requests.get(weather_url, params={"q": city, "appid": api_key})
-#print(response.json())
 data = response.json()
+
 if data["cod"] == 200:
     temp = data["main"]["temp"] - 273
     print("\nLa temperatura en",city, "es:", "{0:.2f}".format(temp), "Celcius\n")
