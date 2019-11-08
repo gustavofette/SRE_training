f6d583cdd5ddaeba2129c937743f87702b2ec598
Gustavo Fette <gustavofette@gmail.com>
2019-11-07 22:01:27 -0300

    Some more chnges to phonebook

--- Mapping/phonebook.py
+++ Mapping/phonebook.py
@@ -1,7 +1,9 @@
 myPhoneBook = {"Alim": "453-213-4567",
                "Navean": "123-456-7890",
-               "Gustavo": "987-654-1230"
-               }
+               "Gustavo": "987-654-1230",
+               "Veronica": "555-555-1234"
+              }
+
 def lookfor():
    looking = input("Name: ")
    found = myPhoneBook.get(looking)
@@ -9,16 +11,19 @@ def lookfor():
        print("%s is not in the phonebook" % looking)
    else:
        print(f' {looking} phone is {myPhoneBook[looking]}')
+
 def addone():
    nname = input("New Name: ")
    pphone = input("Phone: ")
    myPhoneBook[nname] = pphone
+
 def delone():
    nname = input("New Name: ")
    if nname in myPhoneBook:
        del myPhoneBook[nname]
    else:
        print(f'{nname} not in the phonebook')
+
 while True:
    print ("My PhoneBook")
    print ("===============")
@@ -30,17 +35,24 @@ while True:
    print ("5- Quit")
    print ("")
    print ("What do you want to do: ")
+   
    YourChoice = input()
+   
    if YourChoice == "1":
        lookfor()
+   
    elif YourChoice == "2":
        addone()
+   
    elif YourChoice == "3":
        delone()
+   
    elif YourChoice == "4":
        print(myPhoneBook)
+   
    elif YourChoice == "5":
        print ("Bye Bye")
        break
+   
    else:
        print("Stop kidding")
