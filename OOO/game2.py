c97f339c08ec7c8090c1077d520b3f4432edbcee
Gustavo Fette <gustavofette@gmail.com>
2019-11-07 22:04:22 -0300

    Added some programs to new directories to match classes and assignments

---
+++ OOO/game2.py
@@ -0,0 +1,26 @@
+class hero:
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+
+    def attack(self, victim):
+        victim.health -= self.power   
+
+class goblin:
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+        
+    def attack(self, victim):
+        victim.health -= self.power
+
+hero1 = hero(10, 5)
+goblin1 = goblin(6, 2)  
+
+print (goblin1.health)
+hero1.attack(goblin1)
+print(goblin1.health)
+
+print (hero1.health)
+goblin1.attack(hero1)
+print(hero1.health)
