0e5978a2df9f43813d6d5dd3890bdc098ebd1107
Gustavo Fette <gustavofette@gmail.com>
2019-11-11 16:22:47 -0300

    Addigns on Mon Nov 11 16:22:47 -03 2019

--- Regular_Expressions/regex.py
+++ Regular_Expressions/regex.py
@@ -1,5 +1,6 @@
 import pyperclip
 import re
+
 phoneRegex = re.compile(r'''(
     (\d{3}|\(\d{3}\))?                # area code
     (\s|-|\.)?                        # separator
@@ -8,13 +9,16 @@ phoneRegex = re.compile(r'''(
     (\d{4})                           # last 4 digits
     (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
     )''', re.VERBOSE)
+
 emailRegex = re.compile(r'''(
      [a-zA-Z0-9._%+-]+      # username
      @                      # @ symbol
      [a-zA-Z0-9.-]+         # domain name
        (\.[a-zA-Z]{2,4})      # dot-something
        )''', re.VERBOSE)
+
 text = str(pyperclip.paste())
+
 matches = []
 for groups in phoneRegex.findall(text):
     phoneNum = '-'.join([groups[1], groups[3], groups[5]])
@@ -23,9 +27,11 @@ for groups in phoneRegex.findall(text):
         phoneNum += ' x' + groups[8]
     # Append numbers to array
     matches.append(phoneNum)
+
 for groups in emailRegex.findall(text):
     matches.append(groups[0])
 # Copy results to the clipboard.
+
 if len(matches) > 0:
     pyperclip.copy('\n'.join(matches))
     print('Copied to clipboard:')
